4e48b840e3f5bcf52bc12d458b3393e8ca1d46d4
asd4876 <1319995730@qq.com>
2018-04-13 21:01:59 +0800

    some codes

--- helloworld/helloworld/helloworld.cpp
+++ helloworld/helloworld/helloworld.cpp
@@ -7,10 +7,20 @@ void helloworld::print() {
 	cout << "helloworld" << endl;
 }
 
-helloworld::helloworld() {
+void helloworld::print() const {
+	cout << "const helloworld" << endl;
+}
+void helloworld::setNum(int num) {
 	cout << "construct" << endl;
+	num = 100;
+	cout << num<<" "<<this->num << endl;
 }
 
+helloworld::helloworld():num(1),cint(4),ci(num){
+	//num = 1;
+	cout << "construct" << endl;
+}
+int helloworld::si = 2;
 helloworld::~helloworld() {
 	cout << "destruct" << endl;
 }
--- helloworld/helloworld/helloworld.h
+++ helloworld/helloworld/helloworld.h
@@ -1,7 +1,13 @@
 #pragma once
 class helloworld {
 public:
+	void print() const;
+	void setNum(int num);
 	void print();
 	helloworld();
 	~helloworld();
+	int num=3;
+	const int cint;
+	const int& ci;
+	static int si;
 };
